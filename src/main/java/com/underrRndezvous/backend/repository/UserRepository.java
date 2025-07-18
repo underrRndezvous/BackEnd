02d7b99c9733bf30b1864a356de6168018ba9e70
ansdudgh98 <ansdudgh98@naver.com>
2025-07-19 01:59:53 +0900

    fix: confilct resolve

--- src/main/java/com/underrRndezvous/backend/repository/UserRepository.java
+++ src/main/java/com/underrRndezvous/backend/repository/UserRepository.java
@@ -1,7 +1,7 @@
 package com.underrRndezvous.backend.repository;
 
 
-import com.underrRndezvous.backend.domain.User;
+import com.underrRndezvous.backend.domain.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
